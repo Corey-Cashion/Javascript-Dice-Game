e15ebab497f370fd091967aac40e98bf0b7a6b4d
Corey A. Cashion <136931060+Corey-Cashion@users.noreply.github.com>
2024-03-24 16:53:04 -0400

    bug fix

--- public/script.js
+++ public/script.js
@@ -1,15 +1,24 @@
 function rollDice() {
     let goldCoins = 0;
     for (let i = 0; i < 10; i++) {
-        const roll = Math.floor(Math.random() *6) +1;
+        let roll = Math.floor(Math.random() * 6) + 1;
         alert('You rolled a ' + roll + '.');
+
         if (roll === 1) {
             alert('Game over, no more rolls!');
             break;
         }
-        if (roll < 5) {
+
+        if (roll === 5) {
+            alert('You rolled a 5! You get to roll again.');
             continue;
         }
+
+        if (roll === 6) {
+            alert('You rolled a 6! You get to roll again.');
+            continue;
+        }
+
         alert('Congratulations, you win ' + roll + ' gold coins!');
         goldCoins += roll;
     }
